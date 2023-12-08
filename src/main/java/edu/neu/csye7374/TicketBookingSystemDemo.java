edd31df57717de8e0640bb7800908dc17184847f
slowpeace2020 <slowpeace2020@gmail.com>
2023-12-08 16:08:30 -0500

    facade

--- src/main/java/edu/neu/csye7374/TicketBookingSystemDemo.java
+++ src/main/java/edu/neu/csye7374/TicketBookingSystemDemo.java
@@ -5,60 +5,30 @@ import edu.neu.csye7374.ticket_command.Command;
 import edu.neu.csye7374.ticket_decorator.EarlyAccessTicketDecorator;
 import edu.neu.csye7374.ticket_decorator.TicketDrinksDecorator;
 import edu.neu.csye7374.ticket_decorator.TicketInsuranceDecorator;
+import edu.neu.csye7374.ticket_facade.TicketBookingFacade;
 import edu.neu.csye7374.ticket_model.Ticket;
 import edu.neu.csye7374.ticket_model.TicketFactory;
 import edu.neu.csye7374.ticket_state.BookedState;
 import edu.neu.csye7374.ticket_state.TicketState;
+import edu.neu.csye7374.ticket_strategy.PricingStrategy;
+import edu.neu.csye7374.ticket_strategy.RegularPricingStrategy;
 
 public class TicketBookingSystemDemo {
     public static void main(String[] args) {
-        // Create movie ticket using factory pattern
-        Ticket ticket = TicketFactory.getTicket("movie");
 
-        // Use prototype mode to copy the ticket (if needed)
-        Ticket clonedTicket = (Ticket) (ticket).clone();
 
-        // Decorate the ticket with early access
-        Ticket earlyAccessTicketDecorator = new EarlyAccessTicketDecorator(ticket);
-        earlyAccessTicketDecorator.printTicketInfo();
-        System.out.println("add early Access Ticket price: "+earlyAccessTicketDecorator.getPrice());
-        // Further decorate with Insurance package
-        Ticket ticketInsuranceDecorator = new TicketInsuranceDecorator(earlyAccessTicketDecorator);
-        ticketInsuranceDecorator.printTicketInfo();
-        System.out.println("add Insurance ticket price: "+ticketInsuranceDecorator.getPrice());
-
-        ticket.setPrice(ticketInsuranceDecorator.getPrice());
-
-        // Create a command to book the ticket
-        Command bookTicket = new BookTicketCommand(ticket);
-        // Execute the command
-        bookTicket.execute();
-
-        // Update the status of a ticket using status design
-        // Booking the ticket
-        TicketState bookedState = new BookedState();
-        ticket.setState(bookedState);
-        ticket.handleState();
-
-//        // 使用策略模式计算票价
-//        PricingStrategy pricingStrategy = new RegularPricingStrategy();
-//        double price = pricingStrategy.calculatePrice();
-//
 //        // 使用观察者模式通知用户（模拟）
 //        Customer customer = new Customer();
 //        TicketSubject ticketSubject = new TicketSubject();
 //        ticketSubject.attach(customer);
 //        ticketSubject.notifyObservers();
 //
-//        // 使用模板方法模式处理票务
-//        TicketProcessingTemplate ticketProcessing = new ConcertTicketProcessing();
-//        ticketProcessing.processTicket();
+
 //
-//        // 使用门面模式简化票务预订和支付流程
-//        TicketBookingFacade bookingFacade = new TicketBookingFacade();
-//        PaymentDetails paymentDetails = new PaymentDetails();
-//        bookingFacade.bookTicket("movie", paymentDetails);
-//
+//        // Streamline the ticket booking and payment process using facade mode
+        TicketBookingFacade bookingFacade = new TicketBookingFacade();
+        bookingFacade.bookTicket("movie",false);
+
 //        // 使用适配器模式处理支付
 //        PaymentGatewayAdapter paymentAdapter = new PaymentGatewayAdapter(new ExternalPaymentGateway());
 //        paymentAdapter.processPayment(paymentDetails);
