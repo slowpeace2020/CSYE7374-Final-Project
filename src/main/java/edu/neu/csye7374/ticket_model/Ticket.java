fcc30154e41970c7e29759f46794cfa9a1004914
slowpeace2020 <slowpeace2020@gmail.com>
2023-12-08 12:49:47 -0500

    ticket

---
+++ src/main/java/edu/neu/csye7374/ticket_model/Ticket.java
@@ -0,0 +1,49 @@
+package edu.neu.csye7374.ticket_model;
+
+public abstract class Ticket implements Cloneable{
+    protected String title;
+    protected String eventDate;
+    protected double price;
+
+    public Ticket(String title, String eventDate, double price) {
+        this.title = title;
+        this.eventDate = eventDate;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract void printTicketInfo();
+
+    @Override
+    public Ticket clone() {
+        try {
+            return (Ticket) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Clone not supported", e);
+        }
+    }
+}
+
