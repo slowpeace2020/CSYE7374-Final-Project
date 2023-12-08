af7752958d8986c8c12e35dd2ae9fce84be3ff56
LzyAdam <luzhiyuan1997@hotmail.com>
2023-12-08 13:29:27 -0500

    strategy

--- src/main/java/edu/neu/csye7374/Demo.java
+++ src/main/java/edu/neu/csye7374/Demo.java
@@ -2,6 +2,7 @@ package edu.neu.csye7374;
 
 import edu.neu.csye7374.ticket_model.Ticket;
 import edu.neu.csye7374.ticket_model.TicketFactory;
+import edu.neu.csye7374.ticket_strategy.DiscountPricingStrategy;
 import edu.neu.csye7374.ticket_strategy.PricingStrategy;
 import edu.neu.csye7374.ticket_strategy.VIPPricingStrategy;
 
@@ -10,8 +11,10 @@ public class Demo {
         System.out.println("hello");
         Ticket t1=TicketFactory.getTicket("movie");
         PricingStrategy pricingStrategy= new VIPPricingStrategy();
+        PricingStrategy pricingStrategy1= new DiscountPricingStrategy();
 
         System.out.println(pricingStrategy.calculatePrice(t1));
+        System.out.println(pricingStrategy1.calculatePrice(t1));
 
     }
 }
---
+++ src/main/java/edu/neu/csye7374/TicketDecorator/TicketDecorator.java
@@ -0,0 +1,6 @@
+package edu.neu.csye7374.TicketDecorator;
+
+import edu.neu.csye7374.ticket_model.Ticket;
+
+public abstract class TicketDecorator extends Ticket {
+}
